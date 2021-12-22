1e96f31d2a364aa3de270fab407cdc1e53a9f44b
spxrtxk <spartakvasilev1999@gmail.com>
2021-12-22 18:21:14 +0900

    day 3

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,3 +1,8 @@
 $(document).ready(() => {
   $(document).foundation();
 });
+
+$('.mobile__menu').click(function() {
+  $('.mobile-menu__container').toggleClass('mobile-menu-active');
+  $('.mobile__menu').toggleClass('mobile__menu-icon-active');
+});
