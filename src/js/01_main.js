fe4a8446b7bf20e7608c8989a68d9a2c30e91b46
spxrtxk <spartakvasilev1999@gmail.com>
2021-12-26 13:43:59 +0900

    change mob-menu

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -3,7 +3,6 @@ $(document).ready(() => {
 });
 
 $('.mobile__menu').click(function() {
-  $('.mobile-menu__container').toggleClass('mobile-menu-active');
   $('.mobile__menu').toggleClass('mobile__menu-icon-active');
 });
 
