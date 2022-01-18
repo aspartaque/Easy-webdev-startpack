5816b95d8757c0c0843b846bb77418ebfa4b72a6
spxrtxk <spartakvasilev1999@gmail.com>
2022-01-18 17:16:48 +0900

    vue

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -20,7 +20,7 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-/*slider2 with thumbs*/
+/*slider with thumbs*/
 var swiper1 = new Swiper(".mySwiper-1", {
   freeMode: false,
   navigation: {
@@ -43,37 +43,76 @@ $('.l-virtual__descr-open-btn').click(function() {
   $('.l-events__detail-image').css('background-size', '100%');
   $('.l-virtual__descr-open-btn').text('СКРЫТЬ ТЕКСТ');
 });
-// Vue.component("vue-horizontal-calendar", {
-//   // el: '#app',
-//   data: () => ({
-//     choosedDay2: {
-//       dateFormat: "",
-//       year: "",
-//       month: "",
-//       date: "",
-//       day: "",
-//       timestamp: ""
-//     },
-//   }),
-//   components: {
-//     // VueHorizontalCalendar,
-//   },
-//   methods: {
-//     dateChange2(day) {
-//       this.choosedDay2 = day;
-//     },
-//   }
-// });
-// import VueHorizontalCalendar from './vue-horizontal-calendar';
+
 var app = new Vue({
   el: '#app',
   data: () => ({
-    sum: '',
+    //Страница Сделать пожертвование
     prices: ['100', '300', '500', '1000'],
+    //Страница Оформление заказ
+    time: 'Выберите время',
+    hours: ['10:00', '11:00', '12:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00'],
+    cartTime: null,
+    ticket: [
+      {
+        id: 1,
+        name: 'Взрослый',
+        price: 100,
+        quantity: 0,
+        time: ''
+      },
+      {
+        id: 2,
+        name: 'Детский',
+        price: 50,
+        quantity: 0,
+        time: ''
+      }
+    ],
+    ticketQuantityTotal: '',
+    ticketActive: false,
+    total: null,
+    cart: [],
   }),
-  components: {
-    "vue-horizontal-calendar":VueHorizontalCalendar
-  },
+  methods: {
+    addTime(value) {
+      this.cartTime = value
+      // this.cartTime.push(
+      //   this.hours[idx]
+      // )
+      console.log(this.cartTime);
+    },
+    addTicket(idx) {
+      if (!this.cart[idx]) {
+        let data = {
+          id: this.ticket[idx].id,
+          name: this.ticket[idx].name,
+          price: this.ticket[idx].price,
+          quantity: this.ticket[idx].quantity,
+          time: this.cartTime,
+          totalPrice: this.price * this.quantity
+        }
+        this.cart.push (data)
+        // this.cart.push ({
+        //   time: this.cartTime[idx]
+        // })
+      }
+      this.ticketActive = true;
+      this.ticketQuantityTotal = this.cart[idx].quantity += 1;
+      this.ticketQuantityTotal = this.ticket[idx].quantity += 1;
+      this.total = this.cart[idx].price * this.cart[idx].quantity
+      console.log(this.cart);
+    },
+    deleteTicket(idx) {
+      if (this.ticketQuantityTotal <= 1) {
+        this.ticketActive = false;
+      } 
+      if (this.ticketQuantityTotal > 0) {
+        this.ticketQuantityTotal = this.ticket[idx].quantity -= 1;
+        this.total = this.cart[idx].price * this.cart[idx].quantity
+      }
+    },
+  }
 });
 
 
